0f4a97fe16f0cc33f7554ce4cbaace7c954cf4b8
Chateauvisionn <89737965+Chateauvisionn@users.noreply.github.com>
2022-02-24 18:28:29 +0100

    Fixed add_to_path (again)

--- linux/add_to_path.py
+++ linux/add_to_path.py
@@ -1,3 +1,3 @@
 import os
 
-os.system(f"PATH=$PATH:{os.getcwd()}/path/")
+os.system(f"PATH=$PATH:{os.path.abspath('')}/path/")
--- win/add_to_path.py
+++ win/add_to_path.py
@@ -1,3 +1,3 @@
 import os
 
-os.system(f"set PATH=%PATH%;{os.getcwd()}\path\\")
+os.system(f"set PATH=%PATH%;{os.path.abspath('')}\path\\")
